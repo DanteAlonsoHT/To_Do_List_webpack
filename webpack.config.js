008116d4c2e2b26efdcecbdaf207e4e4f05176ac
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-18 03:51:30 -0500

    Fix linters JavaScript and CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"),
-    MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/i,
-                exclude: /node_modules/,
-                use:{
-                    loader: "babel-loader",
-                },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
             },
-            {
-                test: /\.html$/i,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            minimize: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Output Management",
-            template: "./src/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin(),
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
