df51a21c706719f54cfa8e6e75874b70039f4a20
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-19 21:08:21 -0500

    Add function to add a new Task into a button

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import { Methods } from './checkbox.js';
 
 const containerTasks = document.getElementById('container-tasks');
 const buttonAddTask = document.getElementById('button-add-task');
+
+const methods = new Methods();
+
+buttonAddTask.onclick = function add() { methods.addTask(); };
+methods.showTasks();
