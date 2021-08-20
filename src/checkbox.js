4892b92f7a6a8891f6fad665e8e0b71a966ce88d
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-19 21:40:44 -0500

    Fix linters

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,74 +1,73 @@
+/* eslint-disable-next-line */
 export class Methods {
-    addTask() {
-      inputTask = document.getElementById('input-task').value;
-      let inputTask = ''; let lastIndexTasks; let newTask;
-      const formTasks = document.getElementById('form-tasks');
-      if (inputTask.length > 8 && inputTask.length < 32) {
-          //newTask = [[inputTask, 'false']];
-          lastIndexTasks = String(this.getLocalStorage().length)
-          newTask = [{ task_name: inputTask, completed: 'false', index: lastIndexTasks }];
-          this.saveLocalStorage(newTask);
-          this.showTasks();
-          formTasks.reset();
+  addTask() {
+    let inputTask = ''; let lastIndexTasks; let newTask;
+    inputTask = document.getElementById('input-task').value;
+    const formTasks = document.getElementById('form-tasks');
+    if (inputTask.length > 8 && inputTask.length < 32) {
+      // newTask = [[inputTask, 'false']];
+      lastIndexTasks = String(this.getLocalStorage().length);
+      newTask = [{ task_name: inputTask, completed: 'false', index: lastIndexTasks }];
+      this.saveLocalStorage(newTask);
+      this.showTasks();
+      formTasks.reset();
+    }
+  }
+
+  removeTask(index) {
+    let newData;
+    if (index > -1) {
+      newData = this.getLocalStorage();
+      newData.splice(index, 1);
+      this.updateLocalStorage(newData);
+      this.showTasks();
+    }
+  }
+
+  showTasks() {
+    const containerTasks = document.getElementById('container-tasks');
+    containerTasks.innerHTML = '';
+    this.getLocalStorage().forEach((task, index) => {
+      let checked = ''; let lineThrough = '';
+      let buttonRemoveStatus = 'style="display: none;"'; let buttonMoveStatus = '';
+      if (task.completed === 'true') {
+        checked = 'checked';
+        lineThrough = 'style="text-decoration: line-through;"';
+        buttonRemoveStatus = 'style="display: block;"';
+        buttonMoveStatus = 'style="display: none;"';
       }
-    }
-
-    removeTask(index) {
-        let newData; 
-        if (index > -1) {
-          newData = this.getLocalStorage();
-          newData.splice(index, 1);
-          this.updateLocalStorage(newData);
-          this.showTasks();
-        }
-    }
-
-    showTasks() {
-        const containerTasks = document.getElementById('container-tasks');
-        containerTasks.innerHTML = '';
-        this.getLocalStorage().forEach((task, index) => {
-            let checked = ''; let lineThrough = '';
-            let buttonRemoveStatus = 'style="display: none;"'; let buttonMoveStatus = "";
-            if (task['completed'] == "true") {
-                checked = 'checked';
-                lineThrough = 'style="text-decoration: line-through;"'
-                buttonRemoveStatus = 'style="display: block;"'
-                buttonMoveStatus = 'style="display: none;"'
-            }
-            containerTasks.innerHTML
+      containerTasks.innerHTML
                 += `<div class="d-flex justify-content-between border border-light py-0 px-3 ">
                         <div class="d-flex">
                             <input type="checkbox" name="complete-checkbox" id="complete-checkbox-${index}"
                                     class="my-3" ${checked}>
-                            <p class="m-2" id="task-name-${index}" ${lineThrough}>${task['task_name']}</p>
+                            <p class="m-2" id="task-name-${index}" ${lineThrough}>${task.task_name}</p>
                         </div>
                         <button type="button" class="border-0 bg-body button-move" id="button-move-${index}" ${buttonMoveStatus}><i class='bx bx-dots-vertical-rounded'></i></button>
                         <button type="button" class="border-0 bg-body button-remove" id="button-remove-${index}" ${buttonRemoveStatus} onclick="methods.removeTask(${index})"><i class='bx bx-trash'></i></button>
                     </div>`;
-        });
-    }
+    });
+  }
 
-    saveLocalStorage(data) {
-        let allTasks;
-        allTasks = this.getLocalStorage();
-        if (allTasks.length !== 0) {
-          localStorage.setItem('all_tasks', JSON.stringify(allTasks.concat(data)));
-        } else {
-          localStorage.setItem('all_tasks', JSON.stringify(data));
-        }
+  saveLocalStorage(data) {
+    const allTasks = this.getLocalStorage();
+    if (allTasks.length !== 0) {
+      localStorage.setItem('all_tasks', JSON.stringify(allTasks.concat(data)));
+    } else {
+      localStorage.setItem('all_tasks', JSON.stringify(data));
     }
+  }
 
     getLocalStorage = () => {
-        if (localStorage.getItem('all_tasks')) {
-          return JSON.parse(localStorage.getItem('all_tasks'));
-        }
-        else {
-            return [];
-        }
+      if (localStorage.getItem('all_tasks')) {
+        return JSON.parse(localStorage.getItem('all_tasks'));
+      }
+
+      return [];
     }
 
     updateLocalStorage = (data) => {
-        localStorage.clear();
-        localStorage.setItem('all_tasks', JSON.stringify(data));
-    }   
+      localStorage.clear();
+      localStorage.setItem('all_tasks', JSON.stringify(data));
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable-next-line */
 import { Methods } from './checkbox.js';
+/* eslint-disable-next-line */
+import style from './style.css';
 
 const containerTasks = document.getElementById('container-tasks');
 const buttonAddTask = document.getElementById('button-add-task');
@@ -8,34 +11,32 @@ const methods = new Methods();
 buttonAddTask.onclick = function add() { methods.addTask(); };
 methods.showTasks();
 
-let list; let indexList;
-list = methods.getLocalStorage();
+let indexList;
+const list = methods.getLocalStorage();
 
 containerTasks.addEventListener('click', (ev) => {
-  if (ev.target != null && ev.target != 'NaN' && ev.target != '') {
-      if (ev.target.tagName === 'INPUT') {
-        let idTask = ev.target.id.replace("complete-checkbox-","");
-        let buttonRemove = document.getElementById("button-remove-"+idTask);
-        let buttonMove = document.getElementById("button-move-"+idTask);
-        let taskLabel = document.getElementById("task-name-"+idTask);
-        indexList = parseInt(idTask);
-        
-        if (list) {
-            if (list[indexList]['completed'] === "false") {
-                list[indexList]['completed'] = "true";
-                buttonRemove.style.display = "block";
-                buttonMove.style.display = "none";
-                taskLabel.style.textDecoration = "line-through";
-            }
-            else {
-                list[indexList]['completed'] = "false";
-                buttonRemove.style.display = "none"
-                buttonMove.style.display = "block"
-                taskLabel.style.textDecoration = "none";
-            }
-            methods.updateLocalStorage(list);
+  if (ev.target !== null && ev.target !== 'NaN' && ev.target !== '') {
+    if (ev.target.tagName === 'INPUT') {
+      const idTask = ev.target.id.replace('complete-checkbox-', '');
+      const buttonRemove = document.getElementById(`button-remove-${idTask}`);
+      const buttonMove = document.getElementById(`button-move-${idTask}`);
+      const taskLabel = document.getElementById(`task-name-${idTask}`);
+      indexList = parseInt(idTask, 10);
+
+      if (list) {
+        if (list[indexList].completed === 'false') {
+          list[indexList].completed = 'true';
+          buttonRemove.style.display = 'block';
+          buttonMove.style.display = 'none';
+          taskLabel.style.textDecoration = 'line-through';
+        } else {
+          list[indexList].completed = 'false';
+          buttonRemove.style.display = 'none';
+          buttonMove.style.display = 'block';
+          taskLabel.style.textDecoration = 'none';
         }
+        methods.updateLocalStorage(list);
       }
-    };
-  }, false);
-  
+    }
+  }
+}, false);
