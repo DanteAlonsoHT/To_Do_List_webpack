c160f7044f8bbc9fa2a80aa62e612555d1571464
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-19 21:03:03 -0500

    Add showTasks method

--- src/checkbox.js
+++ src/checkbox.js
@@ -21,6 +21,31 @@ export class Methods {
           this.updateLocalStorage(newData);
           this.showTasks();
         }
-      }
+    }
+
+    showTasks() {
+        const containerTasks = document.getElementById('container-tasks');
+        containerTasks.innerHTML = '';
+        this.getLocalStorage().forEach((task, index) => {
+            let checked = ''; let lineThrough = '';
+            let buttonRemoveStatus = 'style="display: none;"'; let buttonMoveStatus = "";
+            if (task['completed'] == "true") {
+                checked = 'checked';
+                lineThrough = 'style="text-decoration: line-through;"'
+                buttonRemoveStatus = 'style="display: block;"'
+                buttonMoveStatus = 'style="display: none;"'
+            }
+            containerTasks.innerHTML
+                += `<div class="d-flex justify-content-between border border-light py-0 px-3 ">
+                        <div class="d-flex">
+                            <input type="checkbox" name="complete-checkbox" id="complete-checkbox-${index}"
+                                    class="my-3" ${checked}>
+                            <p class="m-2" id="task-name-${index}" ${lineThrough}>${task['task_name']}</p>
+                        </div>
+                        <button type="button" class="border-0 bg-body button-move" id="button-move-${index}" ${buttonMoveStatus}><i class='bx bx-dots-vertical-rounded'></i></button>
+                        <button type="button" class="border-0 bg-body button-remove" id="button-remove-${index}" ${buttonRemoveStatus} onclick="methods.removeTask(${index})"><i class='bx bx-trash'></i></button>
+                    </div>`;
+        });
+    }
     
 }
