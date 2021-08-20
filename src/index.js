c52c08234b6afda8596d69ba9884a5aea84d2d39
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-19 21:06:50 -0500

    Import Methods class from checkbox.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,4 @@
-/* eslint-disable-next-line */
-import style from './style.css';
+import { Methods } from './checkbox.js';
 
 const containerTasks = document.getElementById('container-tasks');
-
-const tasks = [
-  {
-    description: 'Making a descriptive README file',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Add linters for the repository',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Improve CSS in the project',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Try to use Webpack for this app',
-    completed: false,
-    index: 3,
-  },
-];
-
-tasks.forEach((value) => {
-  let checked = '';
-  if (value.completed) {
-    checked = 'checked';
-  }
-  containerTasks.innerHTML
-    += `<div class="d-flex justify-content-between border border-light py-0 px-3 ">
-        <div class="d-flex">
-            <input type="checkbox" name="complete-checkbox" id="complete-checkbox"
-                    class="my-3" ${checked}>
-            <p class="m-2">${value.description}</p>
-        </div>
-        <button type="button" class="border-0 bg-body button-move"><i class='bx bx-dots-vertical-rounded'></i></button>
-        <button type="button" class="d-none border-0 bg-body"><i class='bx bx-trash'></i></button>
-    </div>`;
-});
+const buttonAddTask = document.getElementById('button-add-task');
