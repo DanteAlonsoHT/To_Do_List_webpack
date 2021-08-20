e4e5e5ebcb7b8013a71ca12fad0f44197236ee4e
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-19 21:09:10 -0500

    Add function when click a checkbox

--- src/index.js
+++ src/index.js
@@ -7,3 +7,35 @@ const methods = new Methods();
 
 buttonAddTask.onclick = function add() { methods.addTask(); };
 methods.showTasks();
+
+let list; let indexList;
+list = methods.getLocalStorage();
+
+containerTasks.addEventListener('click', (ev) => {
+  if (ev.target != null && ev.target != 'NaN' && ev.target != '') {
+      if (ev.target.tagName === 'INPUT') {
+        let idTask = ev.target.id.replace("complete-checkbox-","");
+        let buttonRemove = document.getElementById("button-remove-"+idTask);
+        let buttonMove = document.getElementById("button-move-"+idTask);
+        let taskLabel = document.getElementById("task-name-"+idTask);
+        indexList = parseInt(idTask);
+        
+        if (list) {
+            if (list[indexList]['completed'] === "false") {
+                list[indexList]['completed'] = "true";
+                buttonRemove.style.display = "block";
+                buttonMove.style.display = "none";
+                taskLabel.style.textDecoration = "line-through";
+            }
+            else {
+                list[indexList]['completed'] = "false";
+                buttonRemove.style.display = "none"
+                buttonMove.style.display = "block"
+                taskLabel.style.textDecoration = "none";
+            }
+            methods.updateLocalStorage(list);
+        }
+      }
+    };
+  }, false);
+  
